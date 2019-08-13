3a015056840b87197dccb6e789c29ff5ccb8c7c4
marcosdevelo <marcosgomezda@gmail.com>
2019-08-13 23:22:09 +0000

    b

--- hello-rigo/src/js/component/navbar.js
+++ hello-rigo/src/js/component/navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 export class Navbar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			dropdownOpen: false
+		};
+	}
+
+	toggle() {
+		this.setState(prevState => ({
+			dropdownOpen: !prevState.dropdownOpen
+		}));
+	}
 	render() {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
@@ -10,27 +25,29 @@ export class Navbar extends React.Component {
 						<img src="https://www.vectorlogo.es/wp-content/uploads/2019/04/logo-vector-star-wars.jpg" />
 					</span>
 				</Link>
-				<div className="ml-auto" />
-				<a
-					className="nav-link dropdown-toggle"
-					href="#"
-					id="navbarDropdownMenuLink"
-					role="button"
-					data-toggle="dropdown"
-					aria-haspopup="true"
-					aria-expanded="false">
-					Favorites
-				</a>
-				<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-					<a className="dropdown-item" href="#">
-						Action
-					</a>
-					<a className="dropdown-item" href="#">
-						Another action
-					</a>
-					<a className="dropdown-item" href="#">
-						Something else here
-					</a>
+
+				<div className="ml-auto">
+					<Dropdown direction="down" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+						<DropdownToggle caret>Favorite</DropdownToggle>
+						<DropdownMenu>
+							<Context.Consumer>
+								{({ store, actions }) => {
+									return store.favorite.map((item, index) => {
+										return (
+											<DropdownItem key={index} header>
+												<span>{item.name}</span>
+												<button
+													className="float-right"
+													onClick={() => actions.removeFavorites(item, index)}>
+													<i className="fas fa-trash-alt" />
+												</button>
+											</DropdownItem>
+										);
+									});
+								}}
+							</Context.Consumer>
+						</DropdownMenu>
+					</Dropdown>
 				</div>
 			</nav>
 		);
